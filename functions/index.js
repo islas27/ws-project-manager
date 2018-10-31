18278f8eaab91c42d9be1bade95640bd5c4ae690
jislas <jislas@insikt.com>
2018-10-31 09:38:27 -0700

    Added CF to track user registrarions into DB

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.registerUserInDb = functions.auth.user().onCreate(async (user) => {
+  const email = user.email
+  const newUser = {
+    email,
+    fullName: user.displayName || email,
+    avatar: user.photoURL || 'https://placeimg.com/640/640/people/',
+    id: user.uid
+  }
+
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const result = await admin.firestore().collection('users').add(newUser)
+  // Log a message that we've succesfully written the message
+  return console.log(`User added with ID: ${result.id} added`)
+});
